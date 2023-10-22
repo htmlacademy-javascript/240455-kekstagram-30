573b36218179feae62cc86c4bcc1de026f2ec1af
Виктория <nikitina.post@gmail.com>
2023-10-22 20:56:27 +0300

    Добавлены вызовы функций

--- js/functions.js
+++ js/functions.js
@@ -2,17 +2,23 @@ function checkLength (string, maxLength) {
   return string.length <= maxLength;
 }
 
+checkLength('проверяемая строка', 20);
+checkLength('проверяемая строка', 18);
+checkLength('проверяемая строка', 10);
+
 function isPalindrom(string) {
   string = string.replaceAll(' ','').toLowerCase();
   let newString = '';
 
   for (let i = string.length - 1; i >= 0; i--) {
-    let char = string[i];
+    const char = string[i];
     newString += char;
   }
 
   return newString === string;
 }
+
+isPalindrom('Лёша на полке клопа нашёл ');
 
 function getNumber (string) {
 
@@ -33,3 +39,12 @@ function getNumber (string) {
 
   return parseInt(result, 10);
 }
+
+getNumber('2023 год');
+getNumber('ECMAScript 2022');
+getNumber('1 кефир, 0.5 батона');
+getNumber('агент 007');
+getNumber('а я томат');
+getNumber(2023);
+getNumber(-1);
+getNumber(1.5);
